564ae215faf1bb61735a111c264d1f7e93ff7dfb
yazumiko <yoongyi7@gmail.com>
2025-08-22 11:40:40 +0700

    complete two features: showAll and totalPaid

--- bin/dart_expense_app.dart
+++ bin/dart_expense_app.dart
@@ -1,7 +1,10 @@
 void showAll(){
-
+print('1. Lunch: 50 bhats');
+print('2. Dinner: 100 bhats');
 }
-void totalPaid(){}
+void totalPaid(){
+  print('Total paid: 150 bhats');
+}
 void main (){
   showAll();
   totalPaid();
